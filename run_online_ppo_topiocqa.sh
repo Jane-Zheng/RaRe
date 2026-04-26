94a0fbd1fa04176521196e4a61bb9efcdcfa665a
Jane-Zheng <122588185+Jane-Zheng@users.noreply.github.com>
2026-04-27 03:18:28 +0800

    Update run_online_ppo_topiocqa.sh

--- run_online_ppo_topiocqa.sh
+++ run_online_ppo_topiocqa.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# cd /home/zhengjiaying/project/RAG-test/RL/PPO
+# cd ./project/RAG-test/RL/PPO
 # nohup bash run_online_ppo.sh
 export CUDA_VISIBLE_DEVICES=0,1
 export WANDB_MODE=offline
@@ -7,17 +7,17 @@ export TOKENIZERS_PARALLELISM=false
 
 python online_ppo_topiocqa.py \
   --seed 42 \
-  --train_file /home/zhengjiaying/project/RAG-test/topicoqa/papers_full_train_re.jsonl \
-  --val_file /home/zhengjiaying/project/RAG-test/topicoqa/devdata/select_papers.jsonl \
-  --cache_dir /home/zhengjiaying/project/RAG-test/RL/PPO/cache \
-  --base_model_name /home/zhengjiaying/project/RAG-test/Flan-T5-large \
-  --policy_model_name /home/zhengjiaying/project/RAG-test/t5-checkpoint/sft_flant5_large_redata_new_topiocqa_gpt_0408_2/checkpoint-8520 \
-  --generator_model_path /home/zhengjiaying/project/RAG-test/Meta-Llama-3-8B-Instruct \
-  --output_dir /home/zhengjiaying/project/RAG-test/RL/PPO/checkpoint/online_ppo_topiocqa_m9r1_0416_1 \
-  --logging_dir /home/zhengjiaying/project/RAG-test/RL/PPO/checkpoint/online_ppo_topiocqa_m9r1_0416_1/logs \
-  --retriever_collection_tsv /home/zhengjiaying/project/Llama2/datasets/topiocqa/selected_wiki_segments.tsv \
-  --retriever_model_path /home/zhengjiaying/project/RAG-test/msmarco-roberta-base-ance-firstp \
-  --retriever_index_path /home/zhengjiaying/project/RAG-test/first_part_test/select_train_topiocqa_faiss_cos.index \
+  --train_file ./project/RAG-test/topicoqa/papers_full_train_re.jsonl \
+  --val_file ./project/RAG-test/topicoqa/devdata/select_papers.jsonl \
+  --cache_dir ./project/RAG-test/RL/PPO/cache \
+  --base_model_name ./project/RAG-test/Flan-T5-large \
+  --policy_model_name ./project/RAG-test/t5-checkpoint/sft_flant5_large_redata_new_topiocqa_gpt_0408_2/checkpoint-8520 \
+  --generator_model_path ./project/RAG-test/Meta-Llama-3-8B-Instruct \
+  --output_dir ./project/RAG-test/RL/PPO/checkpoint/online_ppo_topiocqa_m9r1_0416_1 \
+  --logging_dir ./project/RAG-test/RL/PPO/checkpoint/online_ppo_topiocqa_m9r1_0416_1/logs \
+  --retriever_collection_tsv ./project/Llama2/datasets/topiocqa/selected_wiki_segments.tsv \
+  --retriever_model_path ./project/RAG-test/msmarco-roberta-base-ance-firstp \
+  --retriever_index_path ./project/RAG-test/first_part_test/select_train_topiocqa_faiss_cos.index \
   --retriever_topk 5 \
   --generator_max_docs 1 \
   --retriever_embed_batch_size 32 \
